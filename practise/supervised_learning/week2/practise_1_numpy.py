6ecae79064aefefe4f0878548ae2e00a3fceb66d
yw.hao <yw.hao@trip.com>
2024-11-12 13:56:20 +0800

    feat: 添加部分numpy practise code

--- practise/supervised_learning/week2/practise_1_numpy.py
+++ practise/supervised_learning/week2/practise_1_numpy.py
@@ -1,3 +1,40 @@
 import numpy as np
 import time
 
+a = np.zeros(4)
+print(a)
+b = np.zeros((4,))
+print(b)
+c = np.random.random_sample((4,))
+print(c)
+d = np.arange(3, 7, 1)
+print(d)
+e = np.random.rand(5)
+print(e)
+
+print(e[2].shape)
+
+print('---------------------------------')
+
+a = np.arange(10)
+print(a)
+
+f = a[2:7:1]
+print(f)
+
+f = a[2:7]
+print(f)
+f = a[2:]
+print(f)
+f = a[:7]
+print(f)
+f = a[:]
+print(f)
+f = a[2:7:2]
+print(f)
+print('---------------------------------')
+
+a = np.array([1, 2, 3, 4])
+
+if __name__ == "__main__":
+    pass
